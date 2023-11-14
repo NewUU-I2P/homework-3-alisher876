546d8512d4a04b2b5973864760cf71339329dcb8
alisher876 <147584767+alisher876@users.noreply.github.com>
2023-11-14 22:32:34 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,18 @@
-void problemSolution1(float consumed_water) {
-    float cost;
+float problemSolution1(float consumed_water) {
+    float cost,fixed_amount;
+    fixed_amount = 13;
     // write your code here
-
+    if (consumed_water <= 30) {
+        cost = fixed_amount + (consumed_water * 0.4);
+    }
+    else if (consumed_water > 30 && consumed_water <= 50) {
+        cost = fixed_amount + ((consumed_water - 30) * 0.12) + (30 * 0.4);
+    }
+    else if (consumed_water > 50 && consumed_water <= 60) { 
+        cost = fixed_amount + ((consumed_water - 50) * 1.4) + (30 * 0.4) + (20 * 0.12);
+    }
+    else if (consumed_water > 60) {
+        cost = fixed_amount + ((consumed_water - 60) * 1.5) + (30 * 0.4) + (20 * 0.12) + (10 * 1.4); 
+    }
     return cost;
 }
