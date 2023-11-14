60ce57f98340672c3e8079fb3d38f8f8822ff220
alisher876 <147584767+alisher876@users.noreply.github.com>
2023-11-14 22:32:56 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,13 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
+    if (i > j && i > k) {
+        result = i;
+    } else if (j > k && j > i) {
+        result = j;
+    } else if (k > j && k > i) {
+        result = k;
+    }
 
     return result;
 }
